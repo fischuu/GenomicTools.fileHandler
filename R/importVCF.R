4437f1e8426e6d31a041c743ff947ed54ff6a8a8
fischuu <daniel.fischer@luke.fi>
2020-10-08 22:31:16 +0300

    Option added

--- R/importVCF.R
+++ R/importVCF.R
@@ -7,6 +7,9 @@
 #' In case the logicl flag 'phased' is set to TRUE then the genotypes are expected to be in the format 0|0, otherwise they are expected
 #' to be like 0/1 . If the flag simplify is set genotypes like 0/2 or 1/2 will be set to 0,1,2 coding and multi-alternatives are ignored.
 #' 
+#'  If you would like to extract in addition to the genotype information further any other data from th vcf file formatted in the
+#'  FORMAT field, you can specify their names in the \data{formatFields} option. Currently, it only accepts a single value.
+#' 
 #' The example file was downloaded from here:
 #' 
 #' ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/pilot_data/release/2010_07/exon/snps/
@@ -16,6 +19,7 @@
 #' @param na.seq The missing value definition
 #' @param simplify Logical
 #' @param getInfo Logical
+#' @param formatFields Vector with names
 #' 
 #' @return A vcf object
 #' 
@@ -30,7 +34,7 @@
 #' 
 #' @export
 
-importVCF <- function(file, na.seq="./.", simplify=TRUE, getInfo=FALSE){
+importVCF <- function(file, na.seq="./.", simplify=TRUE, getInfo=FALSE, formatFields=NULL){
 # Necessary variable declaration for Cran checks
   V3 <- NULL
   rn <- NULL
@@ -99,7 +103,12 @@ importVCF <- function(file, na.seq="./.", simplify=TRUE, getInfo=FALSE){
     
 # Extract the genotype information
   genotypes <- vcfBody[, .SD, .SDcols = -c(1:9)]
+  gts <- vcfBody[, .SD, .SDcols = -c(1:9)]
   genotypesInfo <- vcfBody[, .SD, .SDcols = -c(1:9)]
+
+# Get the different format fields
+  gtFormat <- as.matrix(unique(vcfBody[, .SD, .SDcols = 9]))
+  formats <- unlist(strsplit(gtFormat,":"))
   
 # Remove the additional FORMAT fields
   cols = names(genotypes) 
@@ -128,7 +137,6 @@ importVCF <- function(file, na.seq="./.", simplify=TRUE, getInfo=FALSE){
     genotypes[genotypes=="2/2"] <- "02"
   }
   
-  
   genotypesRN <- colnames(genotypes)
   genotypes <- genotypes[, data.table(t(.SD), keep.rownames=TRUE)]  # Takes long, IMPROVE IT!!!
 
@@ -136,8 +144,25 @@ importVCF <- function(file, na.seq="./.", simplify=TRUE, getInfo=FALSE){
   setnames(genotypes, map[[2]])
   rownames(genotypes) <- genotypesRN
   
+# Create the FORMAT list
+  formatMatrixList <- list()
+  if(length(formatFields)>0){
+    for(i in 1:length(formatFields)){
+      tmpFormat <- which(is.element(formats, formatFields[i]))
+      if(length(tmpFormat)==0) stop("Element",formatFields[i],"cannot be found in vcf.")
+      tmpGts <- vcfBody[, .SD, .SDcols = -c(1:9)]
+      n <- tmpFormat
+      pat <- paste0("^([^:]*:){",n-1,"}")
+      cols <- names(tmpGts) 
+      tmpGts[ , (cols) := lapply(.SD, function(x, patInt=pat) {gsub(patInt,"",x)}), .SDcols = cols]
+      tmpGts[ , (cols) := lapply(.SD, function(x) {gsub("\\:.*","",x)}), .SDcols = cols]
+      formatMatrixList[[i]] <- tmpGts
+    }
+    names(formatMatrixList) <- formatFields
+  }
+  
 # Then import the body
-  out <- list(header=header, vcfBody, map=map, genotypes=genotypes, info=info, genotypesInfo=genotypesInfo)
+  out <- list(header=header, vcfBody, map=map, genotypes=genotypes, info=info, genotypesInfo=genotypesInfo, format=formatMatrixList)
   class(out) <- "vcf"
   out
 }
